44a55d27f53696a139ae32a44e130ec6d0d26fbc
bluesky4485 <i@huangke.info>
2017-03-02 22:06:24 +0800

    update to tmux 2.3

--- tmux2.0_install.sh
+++ tmux2.0_install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 mkdir tmux
 cd tmux
-wget https://sourceforge.net/projects/levent/files/libevent/libevent-2.0/libevent-2.0.22-stable.tar.gz
-tar zxvf libevent-2.0.22-stable.tar.gz
-cd libevent-2.0.22-stable
+wget https://github.com/libevent/libevent/releases/download/release-2.1.8-stable/libevent-2.1.8-stable.tar.gz
+tar zxvf libevent-2.1.8-stable.tar.gz
+cd libevent-2.1.8-stable
 ./configure
 make
 make install 
 cd ..
 yum install ncurses-devel -y
-wget https://github.com/tmux/tmux/releases/download/2.2/tmux-2.2.tar.gz
-tar zxvf tmux-2.2.tar.gz
-cd tmux-2.2
+wget https://github.com/tmux/tmux/releases/download/2.3/tmux-2.3.tar.gz
+tar zxvf tmux-2.3.tar.gz
+cd tmux-2.3
 ./configure
 make
 make install 

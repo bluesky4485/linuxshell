7653f022fa3cc537b75da02c1dd6ba3d5db0a018
bluesky4485 <huangke@daoshengtech.com>
2018-06-07 15:47:40 +0800

    add docker-compose

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -5,3 +5,6 @@ yum-config-manager --add-repo  https://download.docker.com/linux/centos/docker-c
 yum install docker-ce
 
 systemctl start docker
+
+curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose

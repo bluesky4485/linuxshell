1532879d4ad7f2e5cd4831bf1255e6641620d94e
bluesky4485 <huangke@daoshengtech.com>
2018-06-08 16:42:08 +0800

    Update dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -18,7 +18,7 @@ yum install git2u nginx tmux2u -y
 
 yum install -y yum-utils device-mapper-persistent-data  lvm2
 yum-config-manager --add-repo  https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce
+yum install docker-ce -y
 
 systemctl start docker
 

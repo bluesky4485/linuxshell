b8261fd637f72a1125328934f7a62131f3b3a2ac
bluesky4485 <huangke@daoshengtech.com>
2018-07-23 16:44:53 +0800

    update docker-compose version

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -22,5 +22,5 @@ yum install docker-ce -y
 
 systemctl start docker
 
-curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose

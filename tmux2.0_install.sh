d92b598d447beed4ddcc619892770a807b34cd3c
bluesky4485 <huangke@daoshengtech.com>
2019-06-27 00:57:45 +0800

    Update tmux2.0_install.sh
    update version

--- tmux2.0_install.sh
+++ tmux2.0_install.sh
@@ -2,17 +2,17 @@
 yum install gcc -y
 mkdir tmux
 cd tmux
-wget https://github.com/libevent/libevent/releases/download/release-2.1.8-stable/libevent-2.1.8-stable.tar.gz
-tar zxvf libevent-2.1.8-stable.tar.gz
-cd libevent-2.1.8-stable
+wget https://github.com/libevent/libevent/releases/download/release-2.1.10-stable/libevent-2.1.10-stable.tar.gz
+tar zxvf libevent-2.1.10-stable.tar.gz
+cd libevent-2.1.10-stable
 ./configure
 make
 make install 
 cd ..
 yum install ncurses-devel -y
-wget https://github.com/tmux/tmux/releases/download/2.8/tmux-2.8.tar.gz
-tar zxvf tmux-2.8.tar.gz
-cd tmux-2.8
+wget https://github.com/tmux/tmux/releases/download/2.9/tmux-2.9.tar.gz
+tar zxvf tmux-2.9.tar.gz
+cd tmux-2.9
 ./configure
 make
 make install 

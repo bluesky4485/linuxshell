8376636de92de6b9815417113fab1bbd2d2f981d
bluesky4485 <huangke@daoshengtech.com>
2019-10-23 15:48:14 +0800

    Create ffmpeg.sh
    centos7 安装ffmpeg

---
+++ ffmpeg.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+yum -y install epel-release
+rpm --import http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro
+rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-1.el7.nux.noarch.rpm
+yum repolist
+yum -y install ffmpeg ffmpeg-devel
+ffmpeg -versio

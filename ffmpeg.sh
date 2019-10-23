23b6386683c77c967b2d5ef7173edbcfd05b0908
bluesky4485 <huangke@daoshengtech.com>
2019-10-23 16:19:22 +0800

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -4,4 +4,9 @@ rpm --import http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro
 rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-1.el7.nux.noarch.rpm
 yum repolist
 yum -y install ffmpeg ffmpeg-devel
-ffmpeg -versio
+ffmpeg -version
+
+
+
+curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+chmod a+rx /usr/local/bin/youtube-dl
